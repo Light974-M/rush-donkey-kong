1a473141c5bf36411334f70f1207f42d1164b122
SEGARIO <nonokurod@gmail.com>
2021-01-06 11:50:41 +0100

    dk 1

---
+++ DonkeyKong Remake/Assets/Scripts/Jump.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Tonneau"))
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ DonkeyKong Remake/Assets/Scripts/TonneauBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TonneauBehaviour : MonoBehaviour
+{
+    public float time = 1.0f;
+
+    public Transform Spawn;
+    public GameObject TonneauGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0.0f)
+        {
+           
+            Vector3 position = new Vector3();
+            Instantiate(TonneauGravity, Spawn.position, Quaternion.identity);
+            GameObject.Destroy(gameObject);
+        }
+    }
+}

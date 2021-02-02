38d0018585067b22f1f4476cf06d9d8beb814cbc
SEGARIO <nonokurod@gmail.com>
2021-02-02 16:19:52 +0100

    Win+Menu
    z

---
+++ DonkeyKong Remake/Assets/Scripts/Buttons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- DonkeyKong Remake/Assets/Scripts/Jump1.cs
+++ DonkeyKong Remake/Assets/Scripts/Jump1.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Jump1 : MonoBehaviour
 {
-    
+
     public Vector3 jump;
     public float jumpForce = 2.0f;
 
@@ -11,7 +12,7 @@ public class Jump1 : MonoBehaviour
     Rigidbody rb;
     void Start()
     {
-        
+
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
@@ -31,6 +32,16 @@ public class Jump1 : MonoBehaviour
 
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("EndLevel"))
+        {
+            SceneManager.LoadScene(2);
         }
     }
 }

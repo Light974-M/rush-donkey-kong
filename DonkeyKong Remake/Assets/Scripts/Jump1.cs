0f767ff2eba26e53e02713ac55b289dec6c6e767
SEGARIO <nonokurod@gmail.com>
2021-02-02 15:17:23 +0100

    Jump
    jump script

---
+++ DonkeyKong Remake/Assets/Scripts/Jump1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump1 : MonoBehaviour
+{
+    
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
